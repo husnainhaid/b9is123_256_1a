fa004eef87ffd86e1cdd76120e32c8b1f66eb13b
husnainhaid <162607254+husnainhaid@users.noreply.github.com>
2025-10-01 02:04:28 -0700

    original work

--- ex2.js
+++ ex2.js
@@ -2,6 +2,15 @@
 //https://projecteuler.net/problem=1
 //ex2 should output the sum of all multiples of a or b less than n
 
-let euler1=()=>alert();
+// Euler Problem #1
+let euler1 = () => {
+    let sum = 0;
+    for (let i = 1; i < 1000; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
+    }
+    alert("Sum of multiples of 3 or 5 below 1000 is: " + sum);
+};
 
 let eulerCustom=()=>alert()
